1b6df37f55cd92b0d1301fd4e225878421126112
JaykumarP1 <jaykumarp1559@gmail.com>
2025-09-18 14:13:11 +0530

    Updates

--- widget.js
+++ widget.js
@@ -1,1 +1,19 @@
-import{c as t,j as d,A as n}from"./assets/app-CH0dwmB0.js";const e=document.createElement("div"),o=e.attachShadow({mode:"open"});e.id="widget";document.body.appendChild(e);t.createRoot(o).render(d.jsxDEV(n,{},void 0,!1,{fileName:"/Users/jay/my/reltme/intr/widget-vite-react-template/src/widget.tsx",lineNumber:10,columnNumber:36},void 0));
+import { c as t, j as d, A as n } from "./assets/app-CH0dwmB0.js";
+const e = document.createElement("div"),
+  o = e.attachShadow({ mode: "open" });
+e.id = "widget";
+document.body.appendChild(e);
+t.createRoot(o).render(
+  d.jsxDEV(
+    n,
+    {},
+    void 0,
+    !1,
+    {
+      fileName: "https://github.com/JaykumarP1/widget/blob/main/src/widget.tsx",
+      lineNumber: 10,
+      columnNumber: 36,
+    },
+    void 0
+  )
+);
